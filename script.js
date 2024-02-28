a5411d4bcc3f126bcb2d14bd4c2bbe138784d404
Ot Roca Tanyà <otroca99@MacBook-Air-de-Ot.local>
2024-02-29 00:15:14 +0100

    Adding a simple counter that user can increase, decrease and reset

---
+++ script.js
@@ -0,0 +1,11 @@
+function resetCounter() {
+  document.getElementById("counterNumber").innerHTML = 0;
+}
+
+function increaseCounter() {
+  document.getElementById("counterNumber").innerHTML++;
+}
+
+function decreaseCounter() {
+  document.getElementById("counterNumber").innerHTML--;
+}
